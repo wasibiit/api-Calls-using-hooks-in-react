3dc2c05308c41c828f637446dd04ada1b5499533
Hammad Ur Rahman <hammadurrahman@users.noreply.github.com>
2019-04-04 11:35:26 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,68 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./App.css";
 import Cards from "./Components/Cards";
 import Form from "./Components/Form";
 import NavBar from "./Components/NavBar";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: ""
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            searchParam: ""
+        };
+    }
 
-  componentDidMount() {
-    this.state = {
-      username: "DevWasi"
-    };
-    this.getUser(this.username);
-  }
+    componentDidMount() {
+        this.state = {
+            username: "DevWasi",
+            searchParam: "DevWasi"
+        };
+        this.getUser(this.username);
+    }
 
-  getUser(username) {
-    fetch(`https://api.github.com/users/${this.state.username}`)
-      .then(res => res.json())
-      .then(data => {
+    getUser(username) {
+        console.log(this.state.username);
+
+        fetch(`https://api.github.com/users/${this.state.username}`)
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    username: data
+                });
+            });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState((state) => ({
+                username: state.searchParam
+            }),
+            function () {
+                this.getUser();
+            });
+
+    };
+
+    handleChange = event => {
         this.setState({
-          username: data
+            searchParam: event.target.value
         });
-      });
-  }
+    };
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.getUser();
-  };
-
-  handleChange = event => {
-    this.setState({
-      username: event.target.value
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <NavBar />
-        <Form
-          username={this.state.username}
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit}
-        />
-        <Cards username={this.state.username} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <NavBar/>
+                <Form
+                    username={this.state.username}
+                    handleChange={this.handleChange}
+                    handleSubmit={this.handleSubmit}
+                />
+                <Cards username={this.state.username}/>
+            </div>
+        );
+    }
 }
 
 export default App;
